be0840bd948c8e01445f3584ea47f22219a0394e
qzz0518 <atm520@vip.qq.com>
2023-11-29 21:37:58 +0900

    update batch rule

--- index.js
+++ index.js
@@ -6,25 +6,16 @@ const {readFileSync} = require("fs");
 const {utils} = require("near-api-js");
 
 async function main() {
-    const config = {
-        networkId: process.env.NETWORK_ID || "mainnet",
-        keyStore: new keyStores.InMemoryKeyStore(),
-        nodeUrl: process.env.NODE_URL,
-    };
-
-    const near = await connect(config);
     let walletData = [];
     const mnemonic = process.env.MNEMONIC;
     const { secretKey } = parseSeedPhrase(mnemonic);
-    const keyPair = KeyPair.fromString(secretKey);
-    await config.keyStore.setKey(config.networkId, process.env.CONTRACT_NAME, keyPair);
-    walletData.push({ privateKey: secretKey, implicitAccountId: process.env.CONTRACT_NAME });
+    const mainWallet = { privateKey: secretKey, implicitAccountId: process.env.CONTRACT_NAME };
     try {
         walletData = JSON.parse(readFileSync('near_wallets.json', 'utf-8'));
     } catch (e) {
         console.log('未找到 near_wallets.json，使用配置的主钱包');
     }
-
+    walletData.push(mainWallet);
     const contractArgs = {
         p: "nrc-20",
         op: "mint",
@@ -34,12 +25,19 @@ async function main() {
 
 
     async function performInscribe(wallet, contractArgs, numberOfTimes) {
+        const config = {
+            networkId: process.env.NETWORK_ID || "mainnet",
+            keyStore: new keyStores.InMemoryKeyStore(),
+            nodeUrl: process.env.NODE_URL,
+        };
+        const keyPair = KeyPair.fromString(wallet.privateKey);
+        await config.keyStore.setKey(config.networkId, wallet.implicitAccountId, keyPair);
+        const near = await connect(config);
+        const account = await near.account(wallet.implicitAccountId);
+        const balance = await account.getAccountBalance();
+        console.log(`账户 ${wallet.implicitAccountId} 余额: ${balance.available}`);
         for (let i = 0; i < numberOfTimes; i++) {
             try {
-                const keyPair = KeyPair.fromString(wallet.privateKey);
-                await config.keyStore.setKey(config.networkId, wallet.implicitAccountId, keyPair);
-                const account = await near.account(wallet.implicitAccountId);
-                const balance = await account.getAccountBalance();
                 if (utils.format.parseNearAmount(balance.available) > 0) {
                     const result = await account.functionCall({
                         contractId: "inscription.near",
