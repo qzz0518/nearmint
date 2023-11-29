23cb56f3f92a3dcca5a9ddaf9d530c3a864f8c12
qzz0518 <atm520@vip.qq.com>
2023-11-29 20:58:12 +0900

    update batch mint

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ require('dotenv').config();
 const nearAPI = require('near-api-js');
 const {connect, KeyPair, keyStores} = nearAPI;
 const {parseSeedPhrase} = require("near-seed-phrase");
+const {readFileSync} = require("fs");
+const {utils} = require("near-api-js");
 
 async function main() {
     const config = {
         networkId: process.env.NETWORK_ID || "mainnet",
         keyStore: new keyStores.InMemoryKeyStore(),
         nodeUrl: process.env.NODE_URL,
-        contractName: process.env.CONTRACT_NAME,
     };
 
-// 连接到 NEAR
     const near = await connect(config);
-    const mnemonic = process.env.MNEMONIC;
-    const {secretKey} = parseSeedPhrase(mnemonic);
-    const keyPair = KeyPair.fromString(secretKey);
-    await config.keyStore.setKey(config.networkId, config.contractName, keyPair);
-    const account = await near.account(config.contractName);
-    // 打印余额
-    console.log("账户余额：", (await account.getAccountBalance()).available);
-    // 定义合约调用参数
+    let walletData = [];
+    try {
+        walletData = JSON.parse(readFileSync('near_wallets.json', 'utf-8'));
+    } catch (e) {
+        console.log('未找到 near_wallets.json，使用配置的主钱包');
+        const mnemonic = process.env.MNEMONIC;
+        const { secretKey } = parseSeedPhrase(mnemonic);
+        const keyPair = KeyPair.fromString(secretKey);
+        await config.keyStore.setKey(config.networkId, process.env.CONTRACT_NAME, keyPair);
+        walletData.push({ privateKey: secretKey, implicitAccountId: process.env.CONTRACT_NAME });
+    }
+
     const contractArgs = {
         p: "nrc-20",
         op: "mint",
@@ -28,23 +32,41 @@ async function main() {
         amt: "100000000"
     };
 
-    const numberOfTimes = 100;
 
-    for (let i = 0; i < numberOfTimes; i++) {
-        try {
-            const result = await account.functionCall({
-                contractId: "inscription.near",
-                methodName: "inscribe",
-                args: contractArgs,
-                gas: "30000000000000",
-                attachedDeposit: "0",
-            });
-            let hash = result.transaction.hash
-            console.log(`第 ${i + 1} 次合约调用结果：`, 'https://nearblocks.io/zh-cn/txns/' + hash);
-        } catch (error) {
-            console.error(`第 ${i + 1} 次合约调用出错：`, error);
+    async function performInscribe(wallet, contractArgs, numberOfTimes) {
+        for (let i = 0; i < numberOfTimes; i++) {
+            try {
+                const keyPair = KeyPair.fromString(wallet.privateKey);
+                await config.keyStore.setKey(config.networkId, wallet.implicitAccountId, keyPair);
+                const account = await near.account(wallet.implicitAccountId);
+                const balance = await account.getAccountBalance();
+                if (utils.format.parseNearAmount(balance.available) > 0) {
+                    const result = await account.functionCall({
+                        contractId: "inscription.near",
+                        methodName: "inscribe",
+                        args: contractArgs,
+                        gas: "30000000000000",
+                        attachedDeposit: "0",
+                    });
+                    let hash = result.transaction.hash;
+                    console.log(`${wallet.implicitAccountId}, 第 ${i + 1} 次操作成功: ${'https://nearblocks.io/zh-cn/txns/' + hash}`);
+                } else {
+                    console.log(`账户 ${wallet.implicitAccountId} 余额不足`);
+                    break; // 如果余额不足，跳出循环
+                }
+            } catch (error) {
+                console.error(`第 ${i + 1} 次操作失败: `, error);
+            }
         }
     }
+    // 10 次操作 是每个钱包都打10次
+    Promise.all(walletData.map(wallet => performInscribe(wallet, contractArgs, 10)))
+        .then(() => {
+            console.log("所有操作完成");
+        })
+        .catch(error => {
+            console.error("操作中有错误发生: ", error);
+        });
 }
 
 main();
